210d1d43cda5096a4a4c4dfb813305cd4745076b
schornT <schorn.t@gmail.com>
2015-07-21 19:51:16 -0600

    Config test. MAY BREAK EVERYTHING!

---
+++ src/pebble-js-app.js
@@ -0,0 +1,27 @@
+//Taking this from my H2G2 face. Not sure where I got that from...
+
+Pebble.addEventListener("showConfiguration",
+  function(e) {
+    //Load the remote config page
+    Pebble.openURL("https://dl.dropboxusercontent.com/u/18421157/Link%20to%20h2g2-config.html");
+  }
+);
+
+Pebble.addEventListener("webviewclosed",
+  function(e) {
+    //Get JSON dictionary
+    var configuration = JSON.parse(decodeURIComponent(e.response));
+    console.log("Configuration window returned: " + JSON.stringify(configuration));
+ 
+    //Send to Pebble, persist there
+    Pebble.sendAppMessage(
+      {"KEY_INVERT": configuration.invert},
+      function(e) {
+        console.log("Sending settings data...");
+      },
+      function(e) {
+        console.log("Settings feedback failed!");
+      }
+    );
+  }
+);
